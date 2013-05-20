58f3cf60af1e3b9372f7a095b2e6a32e0cdb75f3
gailees <gailees@umich.edu>
2013-05-20 08:30:19 -0400

    new iOS project - graffiti

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  graffiti
+//
+//  Created by David Fontenot on 5/20/13.
+//  Copyright (c) 2013 David Fontenot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
